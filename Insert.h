68e3e981d29d9a01244de3fc9b6ae2a9d6b151f6
Akshita <37257506+akshitasingh0706@users.noreply.github.com>
2021-09-17 02:00:46 -0400

    Add files via upload

---
+++ Insert.h
@@ -0,0 +1,14 @@
+//
+//  Insert.h
+//  HW1
+//
+//  Created by AKSHITA SINGH on 9/29/20.
+//  Copyright © 2020 AKSHITA SINGH. All rights reserved.
+//
+
+#ifndef Insert_h
+#define Insert_h
+
+#include <stdio.h>
+
+#endif /* Insert_h */
